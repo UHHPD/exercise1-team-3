28c7f1e1ccc0a5646fc24d5f4ddc7ad56ecf2e04
repl.it user <replituser@example.com>
2020-11-04 15:12:32 +0000

    Add output writing functionality

--- summieren.cc
+++ summieren.cc
@@ -4,14 +4,19 @@
 using namespace std;
 
 int main() {
-// Discord?yes!
-// Already sent invite
-  ifstream fin("daten.txt");
-  int a, b;
-  fin >> a;
-  fin >> b;
-  fin.close();
-  cout << a << " " << b << endl;
 
+  ifstream fin("daten.txt"); // read file daten.txt with the fstream lib
+  ofstream fout("datensumme.txt"); // create output.txt
+  int a, b;
+  for (int i; i < 234; i++){ // loop over the lines of fin
+    fin >> a;
+    fin >> b;
+    fout << a + b << endl; //  store the summ of a and b in fout
+  }
+  fin.close();
+  fout.close(); // close the files
 }
 
+// for index, element in enumerate(some_iterable):
+//  do somthing with the element
+//  use index
