3c94acaf9abf07db8cd2e6027447b4ab86f6857c
repl.it user <replituser@example.com>
2020-11-11 15:59:19 +0000

    Add cout statement

--- summieren.cc
+++ summieren.cc
@@ -8,10 +8,12 @@ int main() {
   ifstream fin("daten.txt"); // read file daten.txt with the fstream lib
   ofstream fout("datensumme.txt"); // create output.txt
   int a, b;
-  for (int i; i < 234; i++){ // loop over the lines of fin
+  for (int i; i < 234; i++) { // loop over the lines of fin
     fin >> a;
     fin >> b;
-    fout << a + b; // store the summ of a and b in fout
+    int result = a + b;
+    cout << result << endl; // print result
+    fout << result << endl; // store the summ of a and b in fout
     if (i != 233) {
       fout << "\r\n";
     }
